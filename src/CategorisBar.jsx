e0ced27067316906fda4b91fdca220592460345a
shahareldar <88107120+shahareldar@users.noreply.github.com>
2023-12-06 20:14:46 +0200

    my first component branch (#2)
    * responsive nav-bar
    
    * add tab by click
    
    * added text to button
    
    * change positions and button text
    
    * edit name by screen
    
    * color text changed

--- src/CategorisBar.jsx
+++ src/CategorisBar.jsx
@@ -3,20 +3,22 @@ import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import './CategoriesBar.css';
+import Button from './Button.jsx';
+import { useState } from 'react';
 
-function samePageLinkNavigation(event) {
-  if (
-    event.defaultPrevented ||
-    event.button !== 0 || // ignore everything but left-click
-    event.metaKey ||
-    event.ctrlKey ||
-    event.altKey ||
-    event.shiftKey
-  ) {
-    return false;
-  }
-  return true;
-}
+// function samePageLinkNavigation(event) {
+//   if (
+//     event.defaultPrevented ||
+//     event.button !== 0 || // ignore everything but left-click
+//     event.metaKey ||
+//     event.ctrlKey ||
+//     event.altKey ||
+//     event.shiftKey
+//   ) {
+//     return false;
+//   }
+//   return true;
+// }
 
 function LinkTab(props) {
   return (
@@ -27,31 +29,72 @@ function LinkTab(props) {
 }
 
 export default function NavTabs() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [tabs, setTabs] = useState([
+    { label: 'count is', href: '#' },
+    { label: 'Page Two', href: '#' },
+  ]);
+  const [newTabText, setNewTabText] = useState('');
+  const [showInput, setShowInput] = useState(false);
 
   const handleChange = (event, newValue) => {
-    // event.type can be equal to focus with selectionFollowsFocus.
-    if (
-      event.type !== 'click' ||
-      (event.type === 'click' && samePageLinkNavigation(event))
-    ) {
-      setValue(newValue);
+    setValue(newValue);
+  };
+
+  const handleAddTab = () => {
+    if (newTabText.trim() !== '') {
+      setTabs([...tabs, { label: newTabText, href: '#' }]);
+      setValue(tabs.length);
+      setNewTabText('');
+      setShowInput(false);
     }
   };
 
   return (
-    <Box className="cbar" sx={{ width: '100%' }}>
+    <Box
+      sx={{
+        width: '55%',
+        display: 'flex',
+        position: 'relative',
+        left: '25vw',
+        alignItems: 'flex-end',
+      }}
+    >
       <Tabs
-        // TabIndicatorProps={{ sx: { backgroundColor: '#646cff' } }}
+        sx={{ width: '99%' }}
+        TabIndicatorProps={{
+          sx: { backgroundColor: '#646cff' },
+        }}
         value={value}
         onChange={handleChange}
         aria-label="nav tabs example"
         className="cbar"
+        variant="scrollable"
+        scrollButtons="auto"
       >
-        <LinkTab label="Page One" href="#" />
-        <LinkTab label="Page Two" href="#" />
-        <LinkTab label="Page Three" href="#" />
+        {tabs.map((tab, index) => (
+          <LinkTab key={index} label={tab.label} href={tab.href} />
+        ))}
       </Tabs>
+      <div
+        style={{ marginLeft: '10px', display: 'flex', alignItems: 'flex-end' }}
+      >
+        <Button
+          style={{ position: 'absolute', right: '0' }}
+          text={'+ add'}
+          onClick={() => setShowInput(true)}
+        />
+        {showInput && (
+          <div>
+            <input
+              type="text"
+              value={newTabText}
+              onChange={(e) => setNewTabText(e.target.value)}
+            />
+            <Button onClick={handleAddTab}>Add Tab</Button>
+          </div>
+        )}
+      </div>
     </Box>
   );
 }
